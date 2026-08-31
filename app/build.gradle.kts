f8142ad644ce292f5c8850f3172a045a2c4ad4c0
cgoxo <goelchitresh13@gmail.com>
2026-08-31 21:51:53 +0530

    Bump to 0.1.2 (versionCode 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.minimal.pdfcreate"
         minSdk = 29
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
     }
 
     signingConfigs {
